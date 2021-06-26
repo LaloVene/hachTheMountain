df1e71b4de8f74cfcd65dfcf991560498d310826
Eduardo Venegas <lalohdez77@gmail.com>
2021-06-26 09:28:45 -0500

    added light theme

--- learn2code/src/app/app-routing.module.ts
+++ learn2code/src/app/app-routing.module.ts
@@ -4,12 +4,22 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
+  },
+  {
+    path: 'sign-in',
+    loadChildren: () =>
+      import('./pages/sign-in/sign-in.module').then((m) => m.SignInPageModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
   },
 ];
 
