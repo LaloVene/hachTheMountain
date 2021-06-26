41f74b7f8e0c8b4dee1c629fbaa3688312c896d2
Eduardo Venegas <lalohdez77@gmail.com>
2021-06-26 10:21:57 -0500

    Added sign in page

--- learn2code/src/app/pages/sign-in/sign-in.page.ts
+++ learn2code/src/app/pages/sign-in/sign-in.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +8,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.page.scss'],
 })
 export class SignInPage implements OnInit {
+  // Form
+  public registerForm: FormGroup;
+  public registerFormSubmitted: boolean = false;
+  public loginForm: FormGroup;
+  public loginFormSubmitted: boolean = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {
+    this.createNewForm();
   }
 
+  createNewForm() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  register() {
+    this.registerFormSubmitted = true;
+    if (this.registerForm.invalid) {
+      this.badForm();
+    } else {
+      console.log(111);
+    }
+  }
+  login() {
+    this.loginFormSubmitted = true;
+    if (this.loginForm.invalid) {
+      this.badForm();
+    } else {
+      console.log(111);
+    }
+  }
+
+  async badForm() {
+    const alert = await this.alertController.create({
+      message: 'Favor de verificar algunos campos',
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            alert.dismiss();
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
 }
