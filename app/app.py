f8bee75fcde2e1e326305486839c1412bca3e1fb
Charbel Breydy Torres <66335475+Buly1601@users.noreply.github.com>
2021-06-26 20:47:49 -0500

    last commit, backend finished

--- app/app.py
+++ app/app.py
@@ -3,6 +3,9 @@ import db
 from flask import Flask, render_template, send_from_directory, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from db import get_db
+from dotenv import dotenv_values
+import jwt
+
 
 # Flask uses load dotenv by default
 app = Flask(__name__)
@@ -265,61 +268,181 @@ def set_language():
         return jsonify({"status": "bad", "message": "missing data"}), 400
     return jsonify({"status": "ok", "message":"", "name": name}), 200
 
-@app.route("/sign-in", methods=["POST"])
+# directories
+@app.route("/sign-in", methods=["GET","POST"])
 def sign_in():
+    """
+    !CHECKED!
+    Function that inserts the new username and password to the sql database.
+    Checks for any error.
+    """
     if request.method == "POST":
-        username = request.form.get("Username")
-        password = request.form.get("Password")
-        db = get_db()
+
+        # storing the posted username and password
+        try:
+            body = request.get_json()
+        except:
+            return jsonify({"status": "bad", "message": "no information provided"}), 401
+
+        try:
+            print(body["username"])
+            password = str(body["password"])
+            username = str(body['username'])
+        except:
+            return jsonify({"status": "bad", "message": "missing data"}), 400
+        db = db.get_db()
         error = None
 
+        # checks for any errors
         if not username:
             error = "Username is required"
         elif not password:
             error = "Password is required"
         elif db.execute(
-            "SELECT * FROM user WHERE Username = ?", (username)
+            "SELECT * FROM user WHERE Username = ?", (username,)
         ).fetchone() != None:
             error = f"user {username} already exists"
 
+        # insert into database
         if error == None:
             db.execute(
-                "INSERT INTO user (Username,Password) values (?,?)",
-                username, generate_password_hash(password))
+                "INSERT INTO user (Username,Password) VALUES (?,?)",
+                (username, generate_password_hash(password),))
             db.commit()
             return f"User {username} created successfully"
+        
+        # returns the error 
         else:
             return error, 418
 
-    # TODO return render_template('index.html', title="main page", url=os.getenv("URL"))
-    # ? html, title?
-    return ""
+    else:
+        return ""
 
-@app.route("/log-in", methods=["POST"])
+@app.route("/log-in", methods=["GET","POST"])
 def log_in():
+    """
+    !CHECKED!
+    Function that checks for existence of the username and validates the 
+    password for loging in the web app.
+    """
     if request.method == "POST":
-        username = request.form.get("Username")
-        password = request.form.get("Password")
-        db = get_db()
-        error = None
 
-        user = db.execute(
-            "SELECT * from user WHERE Username = ?", (username)
+        # gets the username and password posted
+        db = db.get_db()
+        error = None
+        try:
+            body = request.get_json()
+        except:
+            return jsonify({"status": "bad", "message": "no information provided"}), 401
+
+        try:
+            print(body["username"])
+            password = str(body["password"])
+            username = str(body['username'])
+        except:
+            return jsonify({"status": "bad", "message": "missing data"}), 400
+
+        # look for the user in the db
+        user_ = db.execute(
+            "SELECT * FROM user WHERE Username = ?", (username,)
         ).fetchone()
         
-        if not user:
+        # no user match
+        if not user_:
             error = "Incorrect Username or nonexistent"
-        elif not check_password_hash(user["Password"],password):
+        # check password validity
+        elif not check_password_hash(user_["Password"], password):
             error = "Incorrect password"
 
+        # return json token with ok status
         if not error:
-            return "Successful login", 200
+            token = jwt.encode({"username" : username}, "error--", algorithm="HS256")
+            return jsonify({"status" : "ok", "message" : "correct username", "token" : token}),200
+        # return the error
         else:
             return error, 418
-            
-    # TODO return render_template('index.html', title="main page", url=os.getenv("URL"))
-    # ? html, title?
-    return "Ok",200
+    
+    else:
+        return ""
+
+@app.route("/get-language", methods=["GET", "POST"])
+def get_lang():
+    """
+    !CHECKED!
+    Function that takes all of the languages and returns if existing
+    """
+    try:
+        body = request.get_json()
+    except:
+        return jsonify({"status": "bad", "message": "no information provided"}), 401
+
+    try:
+        id_lang = int(body["IdLanguages"])
+        name = str(body['Name'])
+        pic_url = str(body['Pic_url'])
+        desc = str(body['Desc'])
+        example = str(body['Example'])
+        status = 1
+    except:
+        return jsonify({"status": "bad", "message": "missing data"}), 400
+
+    return jsonify({
+        "status": "ok", "name": name, "pic_url" : pic_url, "desc" : desc,
+        "example" : example, "id" : id_lang
+    }), 200
+
+@app.route("/get-topic", methods=["GET", "POST"])
+def get_topic():
+    """
+    !CHECKED!
+    Gets and returns the topic via the language input
+    """
+    try:
+        body = request.get_json()
+    except:
+        return jsonify({"status": "bad", "message": "no information provided"}), 401
+
+    try:
+        id_topic = int(body["IdTopic"])
+        name = str(body['Name'])
+        pic_url = str(body['Pic_url'])
+        id_lang = str(body['Id_Languages'])
+        status = 1
+    except:
+        return jsonify({"status": "bad", "message": "missing data"}), 400
+
+    return jsonify({
+        "status": "ok", "name": name, "pic_url" : pic_url,
+         "id" : id_topic, "id_lang" : id_lang
+    }), 200
+
+@app.route("/get-resources", methods=["GET","POST"])
+def get_resource():
+    """
+    !CHECKED!
+    Function that gets all of the resources available for the topic of the language.
+    """
+    try:
+        body = request.get_json()
+    except:
+        return jsonify({"status": "bad", "message": "no information provided"}), 401
+
+    try:
+        name = str(body['Name'])
+        url = str(body['Url'])
+        id_topic = int(body["Id_Rec"])
+        status = 1
+    except:
+        return jsonify({"status": "bad", "message": "missing data"}), 400
+
+    if body["IdUrl"]:
+        id_resource = int(body["IdUrl"])
+    else:
+        id_resource = int(body["IdVideos"])
+    return jsonify({
+        "status": "ok", "name": name, "Url" : url,
+         "id_topic_resource" : id_topic, "id_resource" : id_resource
+    }), 200
 
 # ! health directory !
 @app.route("/health")
